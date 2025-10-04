57f2f6555dfe5281fb64992d05fcc57cd6583307
melod6 <186506596+melod6@users.noreply.github.com>
2025-10-04 10:53:36 -0400

    Implement REPL

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 func main() {
-	fmt.Println("Hello, World!")
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %v\n", input[0])
+	}
 }
 
 func cleanInput(text string) []string {
-	return strings.Fields(text)
+	return strings.Fields(strings.ToLower(text))
 }
