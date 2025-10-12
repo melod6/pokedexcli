62cdbb8635641fbebf1d7a9aa85275b2d6edde67
melod6 <186506596+melod6@users.noreply.github.com>
2025-10-11 20:44:52 -0400

    Implement reapLoop()

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,13 +1,16 @@
 package pokecache
 
 import (
-	"time"
 	"sync"
+	"time"
 )
 
 type Cache struct {
-	cacheEntries     map[string]cacheEntry
-	mutex            sync.Mutex
+	cacheEntries map[string]cacheEntry
+	mutex        sync.Mutex
+}
+
+func NewCache(interval time.Duration) {
 }
 
 func (c *Cache) Add(key string, val []byte) {
@@ -21,11 +24,20 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	return c.cacheEntries[key], true
 }
 
-func (c *Cache) reapLoop()
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.mutex.Lock()
+		for key, entry := range c.cacheEntries {
+			if time.Now().Sub(entry.createdAt) > interval {
+				delete(c.cacheEntries, key)
+			}
+		}
+		c.mutex.Unlock()
+	}
+}
 
 type cacheEntry struct {
 	createdAt time.Time
-	val []byte
+	val       []byte
 }
-
-
